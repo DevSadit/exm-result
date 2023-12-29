38d0d31ce642d4c39a43a30c8a0d5e72b273b04f
Sadit <153073569+DevSadit@users.noreply.github.com>
2023-12-29 12:30:32 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let marks = 30;
+if (marks>= 80) {
+    console.log (`you got A+`);
+}
+else if (marks >= 60) {
+    console.log (`you got A-`);
+}
+else if (marks <=33) {
+    console.log (`you fail`);
+}
